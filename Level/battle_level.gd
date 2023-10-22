df5a78d584f8bc7007529c14473aef69363b5764
adm9835 <andy_moody_7@yahoo.com>
2023-10-22 12:24:59 -0400

    rename plant to enemy

---
+++ Level/battle_level.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+func _ready():
+	add_user_signal("entered_battle")
+	add_user_signal("exited_battle")
+
+
+func enter_battle():
+	emit_signal("entered_battle")
+
+
+func exit_battle():
+	emit_signal("exited_battle")
--- main.gd
+++ main.gd
@@ -2,27 +2,40 @@ extends Node
 
 @onready var camera_2d = $Camera2D
 @onready var opening_cutscene = $OpeningCutscene
+@onready var battle_level = $BattleLevel
 
 func _ready():
 	opening_cutscene.connect("finished_cutscene", _on_finished_cutscene)
-
+	battle_level.connect("entered_battle", _on_entered_battle)
+	battle_level.connect("exited_battle", _on_exited_battle)
 
 
 func _input(event):
 	if event.is_action_pressed("escape"):
 		$MainMenu.visible = true
 		process_mode = Node.PROCESS_MODE_DISABLED
-		camera_2d.zoom = Vector2(.25,.25)
+		camera_2d.zoom = Vector2(.25, .25)
 		camera_2d.position = Vector2.ZERO
 		zoom_in()
 
+
 func zoom_in():
 	var tween = create_tween()
-	tween.tween_property(camera_2d, "zoom", Vector2(1,1), 1.0)
+	tween.tween_property(camera_2d, "zoom", Vector2(1, 1), 1.0)
+
 
 func zoom_out():
 	var tween = create_tween()
-	tween.tween_property(camera_2d, "zoom", Vector2(.25,.25), 1.0)
+	tween.tween_property(camera_2d, "zoom", Vector2(.25, .25), 1.0)
+
 
 func _on_finished_cutscene():
+	zoom_in()
+
+
+func _on_entered_battle():
+	zoom_out()
+
+
+func _on_exited_battle():
 	zoom_in()

6053bc553675d73943cb2593d247ca1d53cb8211
adm9835 <andy_moody_7@yahoo.com>
2023-10-22 12:59:32 -0400

    rename folders

--- Character/Player/watering_can.gd
+++ Character/Player/watering_can.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
 @onready var animation_player = $AnimationPlayer
-@onready var water_scene = preload("res://Objects/Character/water.tscn")
+@onready var water_scene = preload("res://Character/Player/water.tscn")
 @onready var water_spawner = $WaterSpawner
+
 
 func pour():
 	animation_player.play("pivot")
+
 
 func spawn_water():
 	var newwater = water_scene.instantiate()

82340e16bde5a157de955404d6dbfad9df4da37c
adm9835 <andy_moody_7@yahoo.com>
2023-10-22 11:08:49 -0400

    water particles and animating player

--- Objects/Character/watering_can.gd
+++ Objects/Character/watering_can.gd
@@ -1,6 +1,13 @@
 extends Node2D
 
 @onready var animation_player = $AnimationPlayer
+@onready var water_scene = preload("res://Objects/Character/water.tscn")
+@onready var water_spawner = $WaterSpawner
 
 func pour():
 	animation_player.play("pivot")
+
+func spawn_water():
+	var newwater = water_scene.instantiate()
+	water_spawner.add_child(newwater)
+	newwater.global_position = water_spawner.global_position
